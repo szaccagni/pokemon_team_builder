a44d5691ca6b0b6f72624f198652d9b76e446c7e
szaccagni <51030579+szaccagni@users.noreply.github.com>
2022-06-04 15:40:23 -0400

    add js to color team on hover

--- build_team/static/build_team/main.js
+++ build_team/static/build_team/main.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('.leader_box').forEach( function(leader) {
             leader.onmouseover = () => {
                 const color = leader.querySelector('.leader_color');
-                console.log(color.innerHTML);
                 leader.style.backgroundColor = color.innerHTML;
             }
             leader.onmouseout = () => {
@@ -19,5 +18,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const gym_info = document.querySelector('.gym_info'); 
     if (gym_info) {
         gym_info.style.backgroundColor = gym_info.dataset.color;
+    }
+
+    // check for team container
+    const teams_container = document.querySelector('.teams_container')
+    if (teams_container) {
+        document.querySelectorAll('.teams_link').forEach( function(team) {
+            team.onmouseover = () => {
+                const color = team.querySelector('.team_color');
+                const hex = color.innerHTML
+                const red = parseInt(hex[1]+hex[2],16);
+                const green = parseInt(hex[3]+hex[4],16);
+                const blue = parseInt(hex[5]+hex[6],16);
+                team.style.backgroundColor = `rgba(${red},${green},${blue},.4)`
+            }
+            team.onmouseout = () => {
+                team.style.backgroundColor = '';   
+            }
+        })
+
     }
 });
